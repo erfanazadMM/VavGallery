725cf3e13378168c79a3a382906a959afc681419
erfanazadMM <erfanem@hotmail.com>
2026-06-18 15:35:35 -0700

    basepath fix.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: isProd ? "/VavGallery" : "",
+  assetPrefix: isProd ? "/VavGallery/" : "",
 };
 
 export default nextConfig;
